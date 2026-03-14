f3acef11d13a3f2d561b10417303da35bc116d88
AlexnBray <alexnbray585@gmail.com>
2026-03-14 22:24:10 +1000

    Basic understanding of the API

--- fetcher.py
+++ fetcher.py
@@ -1,24 +1,12 @@
-from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.endpoints import playercareerstats, commonplayerinfo
 from nba_api.live.nba.endpoints import scoreboard
 
 from datetime import datetime
 
 import pandas as pd
 
+def age(player_id: int) -> int: 
+    player_info = commonplayerinfo.CommonPlayerInfo(player_id = player_id)
+    return player_info
 
-
-'''career = playercareerstats.PlayerCareerStats(player_id='203999')
-get_player_career = career.get_data_frames()[0]
-
-career.season_totals_regular_season.get_data_frame()
-
-print(career.season_totals_regular_season.get_data_frame())
-
-'''
-id_input = ""
-
-def career_stats(player_id: int) -> pd.DataFrame: # a pd.DataFrame is a 2D data structure that can hold data of different types (like a spreadsheet or SQL table)
-    career = playercareerstats.PlayerCareerStats(player_id=player_id)
-    return career.season_totals_regular_season.get_data_frame()
-
-print(career_stats(id_input))
+print(age(1630173).get_data_frames())
