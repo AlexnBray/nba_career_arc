62d172466ab5a962810bef8be1802b787fdc3541
AlexnBray <alexnbray585@gmail.com>
2026-03-16 22:19:29 +1000

    Git test

--- fetcher.py
+++ fetcher.py
@@ -8,13 +8,24 @@ import pandas as pd
 
 
 def player_finder(rgx: str):
+
     player_info = players.find_players_by_full_name(regex_pattern= rgx)
+        
     print(player_info[0]['id'])
     print(player_info[0]['full_name'])
     player_career_stats = playercareerstats.PlayerCareerStats(player_info[0]['id'])
+
     return player_career_stats
 
 ply_input = input("Input Player's Full Name: ")
-df = player_finder(ply_input).get_data_frames()[0]["PLAYER_AGE"]
+df = player_finder(ply_input).get_data_frames()[0]
 test = player_finder(ply_input).get_data_frames()[0]
-print(df.columns.tolist())
+
+df_rename = df.rename(columns={"PTS":"PPG"})
+#vvvvvvvvvvv Test line vvvvvvvvvvv
+#print (df.columns)
+#print(df[["PLAYER_AGE", "PTS", "GP", "AST", "DREB"]]) # Columns shows the headers, to list sorts it nicer instead of index(headers) it just prints headers
+#print(df_rename.columns)
+
+
+print(bool([1, 2, 3] or 1/0))
